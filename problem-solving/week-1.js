1be124d8c643097ccab9005894a9f546c665f9b5
Saidamir <69676883+Saidamir-Sh@users.noreply.github.com>
2022-06-08 10:07:34 +0200

    minmax first attempt test

--- problem-solving/week-1.js
+++ problem-solving/week-1.js
@@ -23,5 +23,8 @@ function plusMinus(arrayInt) {
 /*Given five positive integers, find the minimum and maximum values that can be calculated by summing exactly four of the five integers. Then print the respective minimum and maximum values as a single line of two space-separated long integers. */
 
 function miniMaxSum(arr) {
-    
+    const maxNum = Math.max(arr)
+    const minNum = Math.min(arr)
+    console.log(maxNum)
 }
+miniMaxSum(arr)
