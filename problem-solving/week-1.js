723ba691458cd4191d2ca559470343f030846ad6
Saidamir <69676883+Saidamir-Sh@users.noreply.github.com>
2022-06-22 11:42:40 +0200

    Camel case task steps

--- problem-solving/week-1.js
+++ problem-solving/week-1.js
@@ -99,3 +99,32 @@ function breakingRecords(games) {
     return [dMost, dLeast];
 }
 console.log(breakingRecords([9, 10, 5, 20, 20, 4, 5, 2, 25, 1]))
+
+
+/* Camel Case */
+/*
+Camel Case is a naming style common in many programming languages. In Java, method and variable names typically start with a lowercase letter, with all subsequent words starting with a capital letter (example: startThread). Names of classes follow the same pattern, except that they start with a capital letter (example: BlueCar).
+
+Your task is to write a program that creates or splits Camel Case variable, method, and class names.
+
+Input Format
+
+    Each line of the input file will begin with an operation (S or C) followed by a semi-colon followed by M, C, or V followed by a semi-colon followed by the words you'll need to operate on.
+    The operation will either be S (split) or C (combine)
+    M indicates method, C indicates class, and V indicates variable
+    In the case of a split operation, the words will be a camel case method, class or variable name that you need to split into a space-delimited list of words starting with a lowercase letter.
+    In the case of a combine operation, the words will be a space-delimited list of words starting with lowercase letters that you need to combine into the appropriate camel case String. Methods should end with an empty set of parentheses to differentiate them from variable names.
+
+*/
+// Task > creating or splitting  variable, method, and class names
+// * each input starts with (S or C) followed by semicolon , M, C, V followed by semicolon and words
+// * operation is S > split or C > combine
+// * M > method, C > class, V > variable
+// * in the case of S split operation I need to split it into space-delimination list and all words starting with lower case
+// * in the case of C combine operation  I need to combine appropriate  camel case  strings. methods should end with an empty () in order to differentiate variable names
+
+function processData(input) {
+
+    return input
+}
+
